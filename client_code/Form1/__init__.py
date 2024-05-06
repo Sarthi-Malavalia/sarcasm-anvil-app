c4cf21b8360cdb775d1043d7537e9516ce3947c6
5alty.tree3@gmail.com <5alty.tree3@gmail.com>
2024-05-06 17:27:19 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -16,3 +16,20 @@ class Form1(Form1Template):
   def outlined_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     pass
+
+  def Classification_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    pass
+
+def categorise_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Call the google colab function and pass it the iris measurements
+    iris_category = anvil.server.call('predict_iris', 
+                                self.sepal_length.text,
+                                self.sepal_width.text,
+                                self.petal_length.text,
+                                self.petal_width.text)
+    # If a category is returned set our species 
+    if iris_category:
+      self.Classification_label.visible = True
+      self.species_label.text = "the text is " + iris_category.capitalize()
